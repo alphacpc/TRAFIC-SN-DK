0b2a09c173976217a018e2dcf8cad6f6a4f10976
alphacpc <alphacpc@gmail.com>
2023-07-10 15:31:01 +0000

    Premier commit du project

---
+++ src/main/java/sn/trafic/dk/TraficSnAppApplication.java
@@ -0,0 +1,13 @@
+package sn.trafic.dk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficSnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficSnAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/trafic/dk/TraficSnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.trafic.dk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficSnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
