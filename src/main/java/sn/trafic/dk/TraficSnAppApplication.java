34a7fbce1725224fc5b229b466c930fc7f944197
alphacpc <alphacpc@gmail.com>
2023-07-12 20:15:43 +0000

    Started spring app

--- src/main/java/sn/trafic/dk/TraficSnAppApplication.java
+++ src/main/java/sn/trafic/dk/TraficSnAppApplication.java
@@ -2,6 +2,8 @@ package sn.trafic.dk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class TraficSnAppApplication {
@@ -9,5 +11,9 @@ public class TraficSnAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TraficSnAppApplication.class, args);
 	}
-
+	
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
