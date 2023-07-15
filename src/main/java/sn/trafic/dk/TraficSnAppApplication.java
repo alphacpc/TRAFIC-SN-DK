1c951bcfe4c1d42f80f4b70a93e8382b4d77fa91
alphacpc <alphacpc@gmail.com>
2023-07-15 16:59:49 +0000

    Updated project with tested API

--- src/main/java/sn/trafic/dk/TraficSnAppApplication.java
+++ src/main/java/sn/trafic/dk/TraficSnAppApplication.java
@@ -12,7 +12,7 @@ public class TraficSnAppApplication {
 		SpringApplication.run(TraficSnAppApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
+	@GetMapping("/me")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
