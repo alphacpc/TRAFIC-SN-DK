f9508d77c133511be016992b87dc810bd51fca44
alphacpc <alphacpc@gmail.com>
2023-07-21 16:46:10 +0000

    update projet added airflow and spring working well

--- src/main/java/sn/trafic/dk/TraficSnAppApplication.java
+++ src/main/java/sn/trafic/dk/TraficSnAppApplication.java
@@ -2,8 +2,7 @@ package sn.trafic.dk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class TraficSnAppApplication {
@@ -11,9 +10,4 @@ public class TraficSnAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TraficSnAppApplication.class, args);
 	}
-	
-	@GetMapping("/me")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
 }
---
+++ src/main/java/sn/trafic/dk/controllers/Welcome.java
@@ -0,0 +1,16 @@
+package sn.trafic.dk.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("welcome")
+public class Welcome {
+
+	@GetMapping
+	public String SayHello() {
+		return "Hello word";
+	}
+
+}
