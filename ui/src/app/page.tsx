93b7f6946c8881ef056372aef7c8693bd24afd66
alphacpc <alphacpc@gmail.com>
2024-05-22 20:17:57 +0000

    added icons and started design for div panel

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -7,25 +7,76 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-lime-100 p-10">
       
-      <div className="div-panel-stat-trafic w-[10vw] border-2 border-black rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
+      <div className="flex">
+      <div className="div-panel-stat-trafic w-[14vw] border-2 border-bla rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
         <IoTrainOutline size={75}/>
         
+        <p>passagers</p>
         <p className="text-4xl my-4">23 445</p>
         
         <div className="flex items-center">
           
           <div className="p-2 flex items-center">
             <HiOutlineArrowLongDown className="text-red-600" size={20} />
-            <span className="font-bold">23</span>
+            <span className="font-bold">23 Aller</span>
           </div>
 
           <div className="p-2 flex items-center">
             <HiOutlineArrowLongUp className="text-green-600" size={20} />
-            <span className="font-bold">44</span>
+            <span className="font-bold">44 Retour</span>
           </div>
 
         </div>
       </div>
+
+      <div className="div-panel-stat-trafic w-[14vw] mx-10 border-2 border-bla rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
+        <IoBusOutline size={75}/>
+        
+        <p className="mt-4">passagers</p>
+        <p className="text-4xl mb-4">23 445</p>
+        
+        <div className="flex items-center">
+          
+          <div className="p-2 flex items-center">
+            <HiOutlineArrowLongDown className="text-red-600" size={20} />
+            <span className="font-bold">23 Aller</span>
+          </div>
+
+          <div className="p-2 flex items-center">
+            <HiOutlineArrowLongUp className="text-green-600" size={20} />
+            <span className="font-bold">44 Retour</span>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="div-panel-stat-trafic w-[14vw] border-2 border-bla rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
+        <PiRoadHorizon size={75}/>
+        
+
+        <p className="text-4xl mt-4">23 445</p>
+        <p>vehicules</p>
+
+        <div className="flex items-center justify-between">
+          
+          <div className="p-2 flex items-center ">
+            <HiOutlineArrowLongDown className="text-red-600" size={20} />
+            <span className="font-bold">23 Entrant</span>
+          </div>
+
+          <div className="p-2 flex items-center">
+            <HiOutlineArrowLongUp className="text-green-600" size={20} />
+            <span className="font-bold">44 Sortant</span>
+          </div>
+
+        </div>
+      </div>
+
+      
+
+      </div>
+
 
     </main>
   )
