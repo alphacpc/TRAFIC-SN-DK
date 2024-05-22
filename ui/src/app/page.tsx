7b0b296349b67ddee22cba61bd6752d1c7d5dafe
alphacpc <alphacpc@gmail.com>
2024-05-22 20:06:11 +0000

    update div panel stat

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,35 +1,30 @@
 import Image from 'next/image'
 import { IoTrainOutline, IoBusOutline } from "react-icons/io5";
 import { PiRoadHorizon } from "react-icons/pi";
+import { HiOutlineArrowLongDown, HiOutlineArrowLongUp } from "react-icons/hi2";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-red-400 p-10">
-      {/* <h1>Hello world</h1> */}
-      <div className="div-panel-stat-trafic border-2 border-black rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
-        <IoTrainOutline size={100}/>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-lime-100 p-10">
+      
+      <div className="div-panel-stat-trafic w-[10vw] border-2 border-black rounded-md shadow-lg p-4 flex flex-col items-center justify-center">
+        <IoTrainOutline size={75}/>
         
-        <span className="text-2xl">23445</span>
+        <p className="text-4xl my-4">23 445</p>
         
-        <div className="bg-yellow-400 flex">
+        <div className="flex items-center">
           
-          <div className="p-2">
-            <span>l</span>
-            <span>23</span>
+          <div className="p-2 flex items-center">
+            <HiOutlineArrowLongDown className="text-red-600" size={20} />
+            <span className="font-bold">23</span>
           </div>
 
-          <div className="p-2">
-            <span>P</span>
-            <span>44</span>
+          <div className="p-2 flex items-center">
+            <HiOutlineArrowLongUp className="text-green-600" size={20} />
+            <span className="font-bold">44</span>
           </div>
 
         </div>
-      </div>
-
-      <div className="flex">
-        <IoTrainOutline size={100}/>
-        <IoBusOutline size={100}/>
-        <PiRoadHorizon size={100}/>
       </div>
 
     </main>
