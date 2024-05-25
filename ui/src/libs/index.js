061f3c68d486d2afaff62580a2cb0aad887ce2bd
alphacpc <alphacpc@gmail.com>
2024-05-25 18:39:32 +0000

    remove other file and optimize data chart on index.js

--- ui/src/libs/index.js
+++ ui/src/libs/index.js
@@ -1,126 +1,278 @@
-class ApexChart extends React.Component {
-        constructor(props) {
-          super(props);
+const LINE_WITH_LABELS = {
+  series: [
+    {
+      name: "High - 2013",
+      data: [28, 29, 33, 36, 32, 32, 33]
+    },
+    {
+      name: "Low - 2013",
+      data: [12, 11, 14, 18, 17, 13, 13]
+    }
+  ],
+  options: {
+    chart: {
+      height: 350,
+      type: 'line',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2
+      },
+      zoom: {
+        enabled: false
+      },
+      toolbar: {
+        show: false
+      }
+    },
+    colors: ['#77B6EA', '#545454'],
+    dataLabels: {
+      enabled: true,
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    title: {
+      text: 'Average High & Low Temperature',
+      align: 'left'
+    },
+    grid: {
+      borderColor: '#e7e7e7',
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    markers: {
+      size: 1
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+      title: {
+        text: 'Month'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Temperature'
+      },
+      min: 5,
+      max: 40
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'right',
+      floating: true,
+      offsetY: -25,
+      offsetX: -5
+    }
+  },
+}
 
-          this.state = {
-          
-            series: [{
-              data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-                min: 10,
-                max: 60
-              })
-            }],
-            options: {
-              chart: {
-                id: 'fb',
-                group: 'social',
-                type: 'line',
-                height: 160
-              },
-              colors: ['#008FFB']
-            },
-          
-            seriesLine2: [{
-              data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-                min: 10,
-                max: 30
-              })
-            }],
-            optionsLine2: {
-              chart: {
-                id: 'tw',
-                group: 'social',
-                type: 'line',
-                height: 160
-              },
-              colors: ['#546E7A']
-            },
-          
-            seriesArea: [{
-              data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-                min: 10,
-                max: 60
-              })
-            }],
-            optionsArea: {
-              chart: {
-                id: 'yt',
-                group: 'social',
-                type: 'area',
-                height: 160
-              },
-              colors: ['#00E396']
-            },
-          
-            seriesSmall: [{
-              data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-                min: 10,
-                max: 60
-              })
-            }],
-            optionsSmall: {
-              chart: {
-                id: 'ig',
-                group: 'social',
-                type: 'area',
-                height: 160,
-                width: 300
-              },
-              colors: ['#008FFB']
-            },
-          
-            seriesSmall2: [{
-              data: generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
-                min: 10,
-                max: 60
-              })
-            }],
-            optionsSmall2: {
-              chart: {
-                id: 'li',
-                group: 'social',
-                type: 'area',
-                height: 160,
-                width: 300
-              },
-              colors: ['#546E7A']
-            },
-          
-          
-          };
+
+const LINE_DASHED = {
+  series: [{
+    name: "Session Duration",
+    data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+  },
+  {
+    name: "Page Views",
+    data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+  },
+  {
+    name: 'Total Visits',
+    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+  }
+],
+options: {
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [5, 7, 5],
+    curve: 'straight',
+    dashArray: [0, 8, 5]
+  },
+  title: {
+    text: 'Page Statistics',
+    align: 'left'
+  },
+  legend: {
+    tooltipHoverFormatter: function(val, opts) {
+      return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
+    }
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6
+    }
+  },
+  xaxis: {
+    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+      '10 Jan', '11 Jan', '12 Jan'
+    ],
+  },
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val + " (mins)"
+          }
         }
-
-      
-
-        render() {
-          return (
-            <div>
-              <div id="wrapper">
-                <div id="chart-line">
-                <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={160} />
-              </div>
-                <div id="chart-line2">
-                <ReactApexChart options={this.state.optionsLine2} series={this.state.seriesLine2} type="line" height={160} />
-              </div>
-                <div id="chart-area">
-                <ReactApexChart options={this.state.optionsArea} series={this.state.seriesArea} type="area" height={160} />
-              </div>
-                <div class="columns">
-                  <div id="chart-small">
-                <ReactApexChart options={this.state.optionsSmall} series={this.state.seriesSmall} type="area" height={160} width={300} />
-              </div>
-                  <div id="chart-small2">
-                <ReactApexChart options={this.state.optionsSmall2} series={this.state.seriesSmall2} type="area" height={160} width={300} />
-              </div>
-                </div>
-                
-              </div>
-              <div id="html-dist"></div>
-            </div>
-          );
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val + " per session"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          }
         }
       }
+    ]
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  }
+},
+}
 
-      const domContainer = document.querySelector('#app');
-      ReactDOM.render(React.createElement(ApexChart), domContainer);
-    
+
+const COLUMN_BASIC = {
+  series: [{
+    name: 'Net Profit',
+    data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+  }, {
+    name: 'Revenue',
+    data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+  }, {
+    name: 'Free Cash Flow',
+    data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+  }],
+  options: {
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%',
+        endingShape: 'rounded'
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent']
+    },
+    xaxis: {
+      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+    },
+    yaxis: {
+      title: {
+        text: '$ (thousands)'
+      }
+    },
+    fill: {
+      opacity: 1
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return "$ " + val + " thousands"
+        }
+      }
+    }
+  },
+}
+
+const COLUMN_STACKED = {
+  series: [{
+    name: 'PRODUCT A',
+    data: [44, 55, 41, 67, 22, 43]
+  }, {
+    name: 'PRODUCT B',
+    data: [13, 23, 20, 8, 13, 27]
+  }, {
+    name: 'PRODUCT C',
+    data: [11, 17, 15, 15, 21, 14]
+  }, {
+    name: 'PRODUCT D',
+    data: [21, 7, 25, 13, 22, 8]
+  }],
+  options: {
+    chart: {
+      type: 'bar',
+      height: 350,
+      stacked: true,
+      toolbar: {
+        show: true
+      },
+      zoom: {
+        enabled: true
+      }
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        legend: {
+          position: 'bottom',
+          offsetX: -10,
+          offsetY: 0
+        }
+      }
+    }],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        borderRadius: 10,
+        borderRadiusApplication: 'end', // 'around', 'end'
+        borderRadiusWhenStacked: 'last', // 'all', 'last'
+        dataLabels: {
+          total: {
+            enabled: true,
+            style: {
+              fontSize: '13px',
+              fontWeight: 900
+            }
+          }
+        }
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+      categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+        '01/05/2011 GMT', '01/06/2011 GMT'
+      ],
+    },
+    legend: {
+      position: 'right',
+      offsetY: 40
+    },
+    fill: {
+      opacity: 1
+    }
+  },
+
+}
