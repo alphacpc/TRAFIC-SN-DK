42e2d01fc879e4b9517e67db382fd649b1e5dd98
alphacpc <alphacpc@gmail.com>
2024-05-31 19:12:11 +0000

    Line chart working well

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -10,8 +10,6 @@ import dynamic from 'next/dynamic';
 const LineChart = dynamic(() => import('./LineChart'), { ssr: false });
 
 
-
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-lime-100 p-10">
