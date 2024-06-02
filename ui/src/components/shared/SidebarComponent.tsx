9976417b68615c0d5c14f095fc321e9397d01515
alphacpc <alphacpc@gmail.com>
2024-06-02 18:57:48 +0000

    added background linear on icon user

--- ui/src/components/shared/SidebarComponent.tsx
+++ ui/src/components/shared/SidebarComponent.tsx
@@ -10,17 +10,17 @@ const SidebarComponent = () => {
   return (
     <div className="bg-green-60 w-full h-[100vh] flex flex-col items-center relative">
 
-      <div className="w-full flex flex-col items-center my-8 pb-4 border-b-2 border-orange-600">
-        <PiUser size={100} className="border-4 p-2 rounded-full border-green-600 shadow-md"/>
+      <div className="w-full flex flex-col items-center my-8 pb-4 border-b-4 border-slate-200">
+        <PiUser size={100} className="p-2 rounded-full bg-gradient-to-tl from-green-600 to-sky-600 shadow-xl text-white"/>
         <h2 className="font-bold text-lg my-2">Alpha amadou DIALLO</h2>
-        <h4 className="italic">Administrateur</h4>
+        <h4 className="italic text-sm">Administrateur</h4>
       </div>
       
       
       <nav className="h-[35vh]">
         <ul className="bg-red-40 w-full flex flex-col items-baseline h-full justify-between mb-8">
-          { tab.map((item, ind) => <li key={ind}>
-            <Link className="bg-orange-3 w-full cursor-pointer flex items-center" href="/">  
+          { tab.map((item, ind) => <li className="w-full " key={ind}>
+            <Link className="cursor-pointer flex items-center" href="/">  
               <IoHomeOutline className='mr-4'/>
               {item}
             </Link>
@@ -32,7 +32,9 @@ const SidebarComponent = () => {
         </div>
 
 
-        <div className="bg-green-500 absolute bottom-0 left-0 w-full p-4 italic text-base text-justify">
+        <div className="text-white font-bold
+        bg-gradient-to-tl from-green-600 to-sky-600
+        absolute bottom-0 left-0 w-full p-4 italic text-base text-justify">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laborum exercitationem libero.</p>
         </div>
 
